dcfd80c64376ef92b2a2e5b551b1e2e0ad196746
dkteds <95552747+dkteds@users.noreply.github.com>
2021-12-31 06:11:21 +0530

    Add files via upload

---
+++ First.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import streamlit as st
+st.write("Welcome to Streamlit")
+st.write ( "Our First DataFrame")
+st.write ( pd.DataFrame({'Roll':[1,2,3,4],'Name':['A','B','C','D']}))
