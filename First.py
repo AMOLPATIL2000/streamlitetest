4db5a4202744b9eb9b41273560f542c1ed785539
dkteds <95552747+dkteds@users.noreply.github.com>
2021-12-31 06:34:10 +0530

    Update First.py

--- First.py
+++ First.py
@@ -1,20 +1,9 @@
 import streamlit as st
-import plotly.graph_objects as go
-
 
 st.title("Welcome to Streamlit!")
 
-fig = go.Figure(
-    data=[go.Pie(
-        labels=['A', 'B', 'C'],
-        values=[30, 20, 50]
-    )]
+selectbox = st.sidebar.selectbox(
+    "Select yes or no",
+    ["Yes", "No"]
 )
-fig = fig.update_traces(
-    hoverinfo='label+percent',
-    textinfo='value',
-    textfont_size=15
-)
-
-st.write("Pie chart in Streamlit")
-st.plotly_chart(fig)
+st.write(f"You selected {selectbox}")
