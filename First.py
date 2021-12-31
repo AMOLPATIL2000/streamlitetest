438c8b19af33a245dce0ba0d69dad561e11fedea
dkteds <95552747+dkteds@users.noreply.github.com>
2021-12-31 06:13:25 +0530

    Update First.py

--- First.py
+++ First.py
@@ -1,5 +1,5 @@
-import pandas as pd
+# import module
 import streamlit as st
-st.write("Welcome to Streamlit")
-st.write ( "Our First DataFrame")
-st.write ( pd.DataFrame({'Roll':[1,2,3,4],'Name':['A','B','C','D']}))
+ 
+# Title
+st.title("Hello GeeksForGeeks !!!")
