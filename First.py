619d88a47856b0d65bd05c3b967ec2040737c21f
dkteds <95552747+dkteds@users.noreply.github.com>
2021-12-31 06:50:12 +0530

    Update First.py

--- First.py
+++ First.py
@@ -1,140 +1,48 @@
-import numpy as np
-import pandas as pd
 import streamlit as st
-import plotly.graph_objects as go
-from sklearn.datasets import load_iris
+import pandas as pd
+from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 
+st.write("""
+# Simple Iris Flower Prediction App
+This app predicts the **Iris flower** type!
+""")
 
-iris_data = load_iris()
-# separate the data into features and target
-features = pd.DataFrame(
-    iris_data.data, columns=iris_data.feature_names
-)
-target = pd.Series(iris_data.target)
+st.sidebar.header('User Input Parameters')
 
-# split the data into train and test
-x_train, x_test, y_train, y_test = train_test_split(
-    features, target, test_size=0.2, stratify=target
-)
+def user_input_features():
+    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
+    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
+    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
+    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
+    data = {'sepal_length': sepal_length,
+            'sepal_width': sepal_width,
+            'petal_length': petal_length,
+            'petal_width': petal_width}
+    features = pd.DataFrame(data, index=[0])
+    return features
 
+df = user_input_features()
 
-class StreamlitApp:
+st.subheader('User Input parameters')
+st.write(df)
 
-    def __init__(self):
-        self.model = RandomForestClassifier()
+iris = datasets.load_iris()
+X = iris.data
+Y = iris.target
 
-    def train_data(self):
-        self.model.fit(x_train, y_train)
-        return self.model
+clf = RandomForestClassifier()
+clf.fit(X, Y)
 
-    def construct_sidebar(self):
+prediction = clf.predict(df)
+prediction_proba = clf.predict_proba(df)
 
-        cols = [col for col in features.columns]
+st.subheader('Class labels and their corresponding index number')
+st.write(iris.target_names)
 
-        st.sidebar.markdown(
-            '<p class="header-style">Iris Data Classification</p>',
-            unsafe_allow_html=True
-        )
-        sepal_length = st.sidebar.selectbox(
-            f"Select {cols[0]}",
-            sorted(features[cols[0]].unique())
-        )
+st.subheader('Prediction')
+st.write(iris.target_names[prediction])
+#st.write(prediction)
 
-        sepal_width = st.sidebar.selectbox(
-            f"Select {cols[1]}",
-            sorted(features[cols[1]].unique())
-        )
-
-        petal_length = st.sidebar.selectbox(
-            f"Select {cols[2]}",
-            sorted(features[cols[2]].unique())
-        )
-
-        petal_width = st.sidebar.selectbox(
-            f"Select {cols[3]}",
-            sorted(features[cols[3]].unique())
-        )
-        values = [sepal_length, sepal_width, petal_length, petal_width]
-
-        return values
-
-    def plot_pie_chart(self, probabilities):
-        fig = go.Figure(
-            data=[go.Pie(
-                    labels=list(iris_data.target_names),
-                    values=probabilities[0]
-            )]
-        )
-        fig = fig.update_traces(
-            hoverinfo='label+percent',
-            textinfo='value',
-            textfont_size=15
-        )
-        return fig
-
-    def construct_app(self):
-
-        self.train_data()
-        values = self.construct_sidebar()
-
-        values_to_predict = np.array(values).reshape(1, -1)
-
-        prediction = self.model.predict(values_to_predict)
-        prediction_str = iris_data.target_names[prediction[0]]
-        probabilities = self.model.predict_proba(values_to_predict)
-
-        st.markdown(
-            """
-            <style>
-            .header-style {
-                font-size:25px;
-                font-family:sans-serif;
-            }
-            </style>
-            """,
-            unsafe_allow_html=True
-        )
-
-        st.markdown(
-            """
-            <style>
-            .font-style {
-                font-size:20px;
-                font-family:sans-serif;
-            }
-            </style>
-            """,
-            unsafe_allow_html=True
-        )
-        st.markdown(
-            '<p class="header-style"> Iris Data Predictions </p>',
-            unsafe_allow_html=True
-        )
-
-        column_1, column_2 = st.beta_columns(2)
-        column_1.markdown(
-            f'<p class="font-style" >Prediction </p>',
-            unsafe_allow_html=True
-        )
-        column_1.write(f"{prediction_str}")
-
-        column_2.markdown(
-            '<p class="font-style" >Probability </p>',
-            unsafe_allow_html=True
-        )
-        column_2.write(f"{probabilities[0][prediction[0]]}")
-
-        fig = self.plot_pie_chart(probabilities)
-        st.markdown(
-            '<p class="font-style" >Probability Distribution</p>',
-            unsafe_allow_html=True
-        )
-        st.plotly_chart(fig, use_container_width=True)
-
-        return self
-
-
-sa = StreamlitApp()
-sa.construct_app()
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
